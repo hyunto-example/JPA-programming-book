febca139da32a9fca0580f94b6e8d55e39b3b2ff
hyunto <jhyunto@gmail.com>
2018-10-27 18:03:17 +0900

    02장. JPA 시작 완료

--- src/main/java/jpabook/start/Member.java
+++ src/main/java/jpabook/start/Member.java
@@ -1,15 +1,16 @@
 package jpabook.start;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "MEMBER")
 public class Member {
@@ -22,6 +23,6 @@ public class Member {
 	private String username;
 
 	// 매핑 정보가 없는 필드
-	private String age;
+	private Integer age;
 
 }
